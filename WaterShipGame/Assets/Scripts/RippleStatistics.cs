acd604d37bc5a25a99fe56eaed97cab1f564093a
Samuel Hegner <samuel.hegner@gmail.com>
2021-01-19 15:06:50 +0000

    Shapes Added and Ripple Prefab start

---
+++ WaterShipGame/Assets/Scripts/RippleStatistics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ripple Stats", menuName = "Ripple Statistics")]
+public class RippleStatistics : ScriptableObject
+{
+    [Header("Spread Statistics")]
+    [Range(0.1f, 20f)]public float sizeIncreaseSpeed;
+    [Range(0.1f, 20f)] public float maximumRippleSize;
+
+    [Header("Force Statistics")]
+    public float maximumPushForce;
+    public float minimumPushForce;
+}
