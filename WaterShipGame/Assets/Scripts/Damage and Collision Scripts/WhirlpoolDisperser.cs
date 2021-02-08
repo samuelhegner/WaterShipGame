e1f4fe1075028a4c53c5dfd4138183b2550700e0
Samuel Hegner <samuel.hegner@gmail.com>
2021-02-08 14:38:39 +0000

    Whirlpool Prefab

---
+++ WaterShipGame/Assets/Scripts/Damage and Collision Scripts/WhirlpoolDisperser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhirlpoolDisperser : MonoBehaviour, ITouchable
+{
+    [SerializeField] private GameObject parent;
+    [SerializeField] private int numberOfTouchesToDisperse;
+
+    int touchCount = 0;
+    public void OnTouchDown(Vector3 touchPointInWorldSpace)
+    {
+    }
+
+    public void OnTouchHeld(Vector3 touchPointInWorldSpace)
+    {
+
+    }
+
+    public void OnTouchRelease(Vector3 touchPointInWorldSpace)
+    {
+        touchCount++;
+        if (dispersable()) 
+        {
+            disperse();
+        }
+    }
+
+    private bool dispersable()
+    {
+        return touchCount >= numberOfTouchesToDisperse;
+    }
+
+    private void disperse()
+    {
+        Destroy(parent);
+    }
+}
